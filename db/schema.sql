4519d2b1745d9442e3f3fd09ffff243db4354481
booopyhij <mphillips2017@gmail.comgit config --global user.name booopyhijgit config --global user.email mphillips2017@gmail.com>
2023-08-09 18:46:11 -0400

    setting up the boiler plate files

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+-- dropping database
+DROP DATABASE IF EXISTS starchart_db;
+
+
+-- creating database
+CREATE DATABASE starchart_db;
+
+-- dropping db
+DROP DATABASE IF EXISTS mythos_db;
+
+-- creating db
+CREATE DATABASE mythos_db;
---
+++ seeds/index.js
@@ -0,0 +1,12 @@
+
+
+const sequelize = require('../config/connection');
+
+
+const seedAll = async () => {
+
+
+    process.exit(0);
+};
+
+seedAll();
