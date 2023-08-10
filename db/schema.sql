d8d987350ee7f80a4b731b03b351b11a811dafc3
Derek Meduri <derekmeduri@gmail.com>
2023-08-10 18:48:14 -0400

    cleaning up schema

--- db/schema.sql
+++ db/schema.sql
@@ -17,18 +17,4 @@ DROP DATABASE IF EXISTS users_db;
 
 CREATE DATABASE users_db;
 
--- drop tables for user, comment, post if they exist
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS comment;
-DROP TABLE IF EXISTS post;
-
--- create table for user
-CREATE TABLE User (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR (255) NOT NULL,
-    email VARCHAR (255) NOT NULL UNIQUE,
-    password VARCHAR (255) NOT NULL
-);
-
-
 
