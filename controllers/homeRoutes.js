bb905956afe5d82618f7d4af5460d8092e210e1a
Derek Meduri <derekmeduri@gmail.com>
2023-08-14 19:45:37 -0400

    cleaning up 5am spelling errors and adding homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,26 @@
 // to redirect to home page
+const router = require("express").Router();
+const { User } = require("../models");
+const sequelize = require("../config/connection");
+
+//get login from homepage
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  //render handlebars login
+  res.render("login");
+});
+
+//get signup from homepage
+router.get("/signup", (req, res) => {
+  if (req.session.logged_in) {
+    res.direct("/");
+    return;
+  }
+  //render handlebars signup
+  res.render("signup");
+});
+
+module.exports = router;
