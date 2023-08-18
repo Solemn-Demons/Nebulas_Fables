780bc46e22016a9a5ba2cbe73a6986b25df4b80f
betheng <131614496+betheng@users.noreply.github.com>
2023-08-17 20:35:24 -0400

    update constellation.js to accommodate new html dropdown

--- public/assets/js/constellation.js
+++ public/assets/js/constellation.js
@@ -28,40 +28,38 @@
 
 // BETHS CODE TO REVIEW AND BLEND WITH ABOVE WHEN NOT SO TIRED
 // Fetch constellation details based on ID
- async function fetchConstellationDetails(constellationId) {
-    const response = await fetch(`/api/constellation/${constellationId}`);
-    const data = await response.json();
-    return data;
-  }
-  
-  function updateConstellationDetails(constellation) {
-    const constellationName = document.querySelector('.constellation-name');
-    constellationName.textContent = constellation.name;
-  
-    const starsList = document.querySelector('.stars-list');
-    starsList.innerHTML = '';
-    constellation.stars.forEach(star => {
-      const starItem = document.createElement('li');
-      starItem.textContent = star;
-      starsList.appendChild(starItem);
-    });
-  
-    const factsList = document.querySelector('.facts-list');
-    factsList.innerHTML = '';
-    constellation.facts.forEach(fact => {
-      const factItem = document.createElement('li');
-      factItem.textContent = fact;
-      factsList.appendChild(factItem);
-    });
-  }
-  
-  const dropdownItems = document.querySelectorAll('.dropdown-item');
-  dropdownItems.forEach(item => {
-    item.addEventListener('click', async function(event) {
-      event.preventDefault();
-      const selectedId = this.id;
-      const selectedConstellation = await fetchConstellationDetails(selectedId);
-      updateConstellationDetails(selectedConstellation);
-    });
+async function fetchConstellationDetails(constellationId) {
+  const response = await fetch(`/api/constellation/${constellationId}`);
+  const data = await response.json();
+  return data;
+}
+
+function updateConstellationDetails(constellation) {
+  const constellationName = document.querySelector('.constellation-name');
+  constellationName.textContent = constellation.name;
+
+  const starsList = document.querySelector('.stars-list');
+  starsList.innerHTML = '';
+  constellation.stars.forEach(star => {
+    const starItem = document.createElement('li');
+    starItem.textContent = star;
+    starsList.appendChild(starItem);
   });
-   
+
+  const factsList = document.querySelector('.facts-list');
+  factsList.innerHTML = '';
+  constellation.facts.forEach(fact => {
+    const factItem = document.createElement('li');
+    factItem.textContent = fact;
+    factsList.appendChild(factItem);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const selectEl = document.getElementById('constellationOption');
+  selectEl.addEventListener('change', async function() {
+    const selectedId = this.options[this.selectedIndex].value; // Get selected option value
+    const selectedConstellation = await fetchConstellationDetails(selectedId);
+    updateConstellationDetails(selectedConstellation);
+  });
+});
