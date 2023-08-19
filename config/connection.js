46968b018bb64cfb90204e94fb37e1c3c1956ce4
betheng <131614496+betheng@users.noreply.github.com>
2023-08-19 16:29:58 -0400

    databaseurl

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,19 @@
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
+let sequelize;
 
-
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'mysql',
-    });
-
-module.exports = sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
