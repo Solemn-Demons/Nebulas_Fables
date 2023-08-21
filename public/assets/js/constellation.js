2d1856dded86cd5d6fca4ad9dfc66ba68ec58983
betheng <131614496+betheng@users.noreply.github.com>
2023-08-20 23:21:41 -0400

    empty

--- public/assets/js/constellation.js
+++ public/assets/js/constellation.js
@@ -1,31 +1,3 @@
-// constellationSearch();
-
-// var divEl = document.getElementById('starchartSearch');
-
-// function constellationSearch(userSearch)  {
-//     $('#searchBtn').on('click', function async (event) {
-//         var searchInput = $('#user-search').val().trim();
-
-//         if (loggedIn) {
-//             const response = await fetch('/api/constellation', {
-//                 method: 'GET',
-//                 body: JSON.stringify(constellationData),
-//                 headers: {'Content-type': 'application/json'},
-//             })
-//             .then(function (data)
-//             {
-//                 console.log(data);
-//                 var constellationResponse = data;
-//                 $('#starchartSearch').empty();
-//                 console.log(constellationResponse);
-
-//             })
-//         } else {
-//             alert('Please Login');
-//         };
-//     });
-// };
-
 //const constellationId = 
 
 
